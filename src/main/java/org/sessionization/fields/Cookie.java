94d4028416550e7d5ac3691490de992d84b700af
kb2623 <roxor1992@gmail.com>
2015-11-25 08:22:11 +0100

    Logicno zdruzevanje razredov.

--- src/main/java/org/sessionization/fields/Cookie.java
+++ src/main/java/org/sessionization/fields/Cookie.java
@@ -108,3 +108,141 @@ public class Cookie implements LogField {
 		return result;
 	}
 }
+
+@Entity
+@Cacheable
+class CookiePair {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String value;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private CookieKey key;
+
+	public CookiePair() {
+		id = null;
+		value = null;
+		key = null;
+	}
+
+	public CookiePair(String key, String value) {
+		id = null;
+		this.value = value;
+		this.key = ClassPool.getObject(CookieKey.class, key);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public CookieKey getKey() {
+		return key;
+	}
+
+	public void setKey(CookieKey key) {
+		this.key = key;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		CookiePair that = (CookiePair) o;
+
+		if (getId() != null ? !getId().equals(that.getId()) : that.getId() != null) return false;
+		if (getValue() != null ? !getValue().equals(that.getValue()) : that.getValue() != null) return false;
+		if (getKey() != null ? !getKey().equals(that.getKey()) : that.getKey() != null) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = getId() != null ? getId().hashCode() : 0;
+		result = 31 * result + (getValue() != null ? getValue().hashCode() : 0);
+		result = 31 * result + (getKey() != null ? getKey().hashCode() : 0);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return key.toString() + " = " + value;
+	}
+}
+
+@Entity
+@Cacheable
+class CookieKey {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+
+	public CookieKey() {
+		id = null;
+		name = null;
+	}
+
+	public CookieKey(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		CookieKey cookieKey = (CookieKey) o;
+
+		if (getId() != null ? !getId().equals(cookieKey.getId()) : cookieKey.getId() != null) return false;
+		if (getName() != null ? !getName().equals(cookieKey.getName()) : cookieKey.getName() != null) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = getId() != null ? getId().hashCode() : 0;
+		result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
--- src/main/java/org/sessionization/fields/UriQuery.java
+++ src/main/java/org/sessionization/fields/UriQuery.java
@@ -97,3 +97,136 @@ public class UriQuery implements LogField {
 		return result;
 	}
 }
+
+@Entity
+@Cacheable
+class UriQueryPair {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String value;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private UriQueryKey key;
+
+	public UriQueryPair() {
+		id = null;
+		value = null;
+		key = null;
+	}
+
+	public UriQueryPair(String key, String value) {
+		id = null;
+		this.value = value;
+		this.key = ClassPool.getObject(UriQueryKey.class, key);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public UriQueryKey getKey() {
+		return key;
+	}
+
+	public void setKey(UriQueryKey key) {
+		this.key = key;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		UriQueryPair that = (UriQueryPair) o;
+		if (getId() != null ? !getId().equals(that.getId()) : that.getId() != null) return false;
+		if (getValue() != null ? !getValue().equals(that.getValue()) : that.getValue() != null) return false;
+		if (getKey() != null ? !getKey().equals(that.getKey()) : that.getKey() != null) return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = getId() != null ? getId().hashCode() : 0;
+		result = 31 * result + (getValue() != null ? getValue().hashCode() : 0);
+		result = 31 * result + (getKey() != null ? getKey().hashCode() : 0);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return key.toString() + " = " + value;
+	}
+}
+
+@Entity
+@Cacheable
+class UriQueryKey {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+
+	public UriQueryKey() {
+		id = null;
+		name = null;
+	}
+
+	public UriQueryKey(String name) {
+		this.name = name;
+		id = null;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		UriQueryKey that = (UriQueryKey) o;
+		if (getId() != null ? !getId().equals(that.getId()) : that.getId() != null) return false;
+		if (getName() != null ? !getName().equals(that.getName()) : that.getName() != null) return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = getId() != null ? getId().hashCode() : 0;
+		result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
