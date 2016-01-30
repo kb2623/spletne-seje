f2c17fbba3809e5fb6f4921997173751b17a6a61
klemen <roxor1992@gmail.com>
2016-01-30 22:47:34 +0100

    Popravek imena atributa v tabeli.

--- src/main/java/org/sessionization/parser/datastruct/PageViewAbs.java
+++ src/main/java/org/sessionization/parser/datastruct/PageViewAbs.java
@@ -18,7 +18,7 @@ import java.util.List;
 public abstract class PageViewAbs implements TimePoint, HibernateUtil.HibernateTable {
 
 	@OneToMany(cascade = CascadeType.ALL)
-	@JoinTable(joinColumns = @JoinColumn(name = "pageview_id"))
+	@JoinTable(joinColumns = @JoinColumn(name = "pageView_id"))
 	protected List<RequestAbs> requests;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
